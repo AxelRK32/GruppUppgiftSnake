c80c12150127bdff2164dec371686c672bbf86df
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2022-11-17 13:55:02 +0100

    Added placeholder buttons and added exit functionality to quit button

--- src/main/java/org/example/FormName.java
+++ src/main/java/org/example/FormName.java
@@ -1,24 +1,31 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FormName extends JFrame {
     private JPanel MenuTop;
-    private JPanel MenuBottomLeft;
     private JPanel MenuBottomRight;
     private JPanel MenuTopRight;
-    private JPanel MenuTopLeft;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JPanel MenuLeft;
+    private JButton startGameButton;
+    private JButton settingsMapChoiceButton;
     private JTextArea Instructions;
     private JPanel PanelName;
+    private JButton quitButton;
+    private JButton highScoresButton;
 
     FormName() {
         super("Gruppuppgift Snake");
         this.setContentPane(this.PanelName);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
