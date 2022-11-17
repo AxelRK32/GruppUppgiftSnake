7064dbb5bf0a231b0323cac5ea8b61874cde3683
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2022-11-17 13:33:29 +0100

    Added basic menu and code to start the program in Main

--- src/main/java/org/example/FormName.java
+++ src/main/java/org/example/FormName.java
@@ -1,4 +1,24 @@
 package org.example;
 
-public class FormName {
+import javax.swing.*;
+
+public class FormName extends JFrame {
+    private JPanel MenuTop;
+    private JPanel MenuBottomLeft;
+    private JPanel MenuBottomRight;
+    private JPanel MenuTopRight;
+    private JPanel MenuTopLeft;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextArea Instructions;
+    private JPanel PanelName;
+
+    FormName() {
+        super("Gruppuppgift Snake");
+        this.setContentPane(this.PanelName);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
 }
